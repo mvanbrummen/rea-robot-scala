f352eaa2acad34805801420c7fb9043c00e78716
Michael Van Brummen <mvanbrummen@Michaels-MBP.gateway>
2017-05-14 09:41:55 +1000

    Changes

--- ToyRobotSimulation.scala
+++ ToyRobotSimulation.scala
@@ -11,7 +11,7 @@ object ToyRobotSimulation {
         case "LEFT" => rotateLeft()
         case "RIGHT" => rotateRight()
         case "REPORT" => report()
-        case "QUIT" => running = false
+        case "QUIT" | ":Q" => running = false
         case _ => println("What you talkin' 'bout Willis?!")
       }
     }
@@ -20,15 +20,40 @@ object ToyRobotSimulation {
   object Direction extends Enumeration {
     type Direction = Value
     val North, East, South, West = Value
+
+    def rotateRight(dir: Direction) = dir match {
+      case North => East
+      case East => South
+      case South => West
+      case West => North
+    }
+
+    def rotateLeft(dir: Direction) = dir match {
+      case North => West
+      case West => South
+      case South => East
+      case East => North
+    }
+
+    def translation(dir: Direction) = dir match {
+      case North => Point(0, 1)
+      case East => Point(1, 0)
+      case South => Point(0, -1)
+      case West => Point(-1, 0)
+    }
   }
   import Direction._
 
-  case class Point(val x: Int, val y: Int)
+  case class Point(val x: Int, val y: Int) {
+    def +(that: Point) = Point(x + that.x, y + that.y)
+  }
 
   trait Surface {
     def withinBounds(p: Point): Boolean
   }
   case class Table(val min: Point, val max: Point) extends Surface {
+    require(min.x < max.x && min.y < max.y, "Min point must be less than max point")
+
     def withinBounds(p: Point): Boolean = (min.y to max.y contains p.y) && (min.x to max.x contains p.x)
   }
 
@@ -42,12 +67,12 @@ object ToyRobotSimulation {
     }
 
     def unapply(s: String): Option[(Point, Direction)] = s match {
-      case r"PLACE (\d+)${x}\,(\d+)${y}\,(North|East|South|West)${direction}" => Some(Point(x.toInt, y.toInt), Direction withName direction)
+      case r"PLACE (\d+)${x}\,(\d+)${y}\,(NORTH|EAST|SOUTH|WEST)${direction}" => Some(Point(x.toInt, y.toInt), Direction withName direction.toLowerCase.capitalize)
       case _ => None
     }
   }
 
-  var robot: Robot = PlacedRobot(Point(0,0), North)
+  var robot: Robot = UnplacedRobot()
   val table: Surface = Table(Point(0, 0), Point(4, 4))
 
   def place(position: Point, facing: Direction) = if (table.withinBounds(position)) robot = PlacedRobot(position, facing)
@@ -58,26 +83,17 @@ object ToyRobotSimulation {
   }
 
   def move() = robot = robot match {
-    case PlacedRobot(position, North) => PlacedRobot(Point(position.x, position.y + 1), North)
-    case PlacedRobot(position, East) => PlacedRobot(Point(position.x + 1, position.y), East)
-    case PlacedRobot(position, South) => PlacedRobot(Point(position.x, position.y - 1), South)
-    case PlacedRobot(position, West) => PlacedRobot(Point(position.x - 1, position.y), West)
-    case r @ UnplacedRobot() => r
+    case PlacedRobot(pos, dir) if table withinBounds pos + Direction.translation(dir) => PlacedRobot(pos + Direction.translation(dir), dir)
+    case r @ _ => r
   }
 
   def rotateRight() = robot = robot match {
-    case PlacedRobot(position, North) => PlacedRobot(position, East)
-    case PlacedRobot(position, East) => PlacedRobot(position, South)
-    case PlacedRobot(position, South) => PlacedRobot(position, West)
-    case PlacedRobot(position, West) => PlacedRobot(position, North)
+    case PlacedRobot(position, direction) => PlacedRobot(position, Direction.rotateRight(direction))
     case r @ UnplacedRobot() => r
   }
 
   def rotateLeft() = robot = robot match {
-    case PlacedRobot(position, North) => PlacedRobot(position, West)
-    case PlacedRobot(position, West) => PlacedRobot(position, South)
-    case PlacedRobot(position, South) => PlacedRobot(position, East)
-    case PlacedRobot(position, East) => PlacedRobot(position, North)
+    case PlacedRobot(position, direction) => PlacedRobot(position, Direction.rotateLeft(direction))
     case r @ UnplacedRobot() => r
   }
 
