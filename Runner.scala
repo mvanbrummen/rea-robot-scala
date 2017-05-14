66b562c1a74496dcafa19a6ea74c0fce3fd4ba4d
Michael Van Brummen <mvanbrummen@Michaels-MBP.gateway>
2017-05-14 11:23:41 +1000

    Refactoring

--- Runner.scala
+++ Runner.scala
@@ -1,6 +1,31 @@
-object Runner {
-  def main(args: Array[String]) = {
-    val robotSim = new ToyRobotSimulation(Table(Point(0,0), Point(4,4)))
-    robotSim.run()
+object Runner extends App {
+  val robotSim = new ToyRobotSimulation(Table(Point(0,0), Point(4,4)))
+  var running = true
+  val PROMPT = "> "
+
+  while(running) {
+    print(PROMPT)
+    scala.io.StdIn.readLine() match {
+      case PlaceCommand(position, facing) => robotSim.place(position, facing)
+      case "MOVE" => robotSim.move()
+      case "LEFT" => robotSim.rotateLeft()
+      case "RIGHT" => robotSim.rotateRight()
+      case "REPORT" => robotSim.report()
+      case "QUIT" | ":Q" => running = false
+      case _ => println("Whachu' talkin' 'bout Willis?!")
+    }
+  }
+
+  object PlaceCommand {
+    import Direction._
+
+    implicit class Regex(sc: StringContext) {
+      def r = new util.matching.Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
+    }
+
+    def unapply(s: String): Option[(Point, Direction)] = s match {
+      case r"PLACE (\d+)${x}\,(\d+)${y}\,(NORTH|EAST|SOUTH|WEST)${direction}" => Some(Point(x.toInt, y.toInt), Direction withName direction.toLowerCase.capitalize)
+      case _ => None
+    }
   }
 }
--- ToyRobotSimulation.scala
+++ ToyRobotSimulation.scala
@@ -6,67 +6,25 @@ case class UnplacedRobot() extends Robot
 
 class ToyRobotSimulation(val table: Surface) {
   var robot: Robot = UnplacedRobot()
-  var running = true
 
-  def run() = {
-    val PROMPT = "> "
+  def place(position: Point, facing: Direction) = robot = if (table withinBounds position) PlacedRobot(position, facing) else UnplacedRobot()
 
-    while(running) {
-      print(PROMPT)
-      robot = scala.io.StdIn.readLine() match {
-        case PlaceCommand(position, facing) => place(position, facing)
-        case "MOVE" => move()
-        case "LEFT" => rotateLeft()
-        case "RIGHT" => rotateRight()
-        case "REPORT" => report()
-        case "QUIT" | ":Q" => quit()
-        case _ => unknownCommand()
-      }
-    }
-  }
-
-  object PlaceCommand {
-    implicit class Regex(sc: StringContext) {
-      def r = new util.matching.Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
-    }
-
-    def unapply(s: String): Option[(Point, Direction)] = s match {
-      case r"PLACE (\d+)${x}\,(\d+)${y}\,(NORTH|EAST|SOUTH|WEST)${direction}" => Some(Point(x.toInt, y.toInt), Direction withName direction.toLowerCase.capitalize)
-      case _ => None
-    }
-  }
-
-  def quit() = {
-    running = false
-    UnplacedRobot()
-  }
-
-  def unknownCommand() = {
-    println("What you talkin' 'bout Willis?!")
-    robot
-  }
-
-  def place(position: Point, facing: Direction) = if (table withinBounds position) PlacedRobot(position, facing) else UnplacedRobot()
-
-  def report() = {
-    robot match {
+  def report() = robot match {
       case PlacedRobot(position, facing) => println(s"${position.x},${position.y},${facing.toString()}")
       case UnplacedRobot() => // remain silent
-    }
-    robot
   }
 
-  def move() = robot match {
+  def move() = robot = robot match {
     case PlacedRobot(pos, dir) if table withinBounds pos + Direction.translation(dir) => PlacedRobot(pos + Direction.translation(dir), dir)
     case r @ _ => r
   }
 
-  def rotateRight() = robot match {
+  def rotateRight() = robot = robot match {
     case PlacedRobot(position, direction) => PlacedRobot(position, Direction.rotateRight(direction))
     case r @ UnplacedRobot() => r
   }
 
-  def rotateLeft() = robot match {
+  def rotateLeft() = robot = robot match {
     case PlacedRobot(position, direction) => PlacedRobot(position, Direction.rotateLeft(direction))
     case r @ UnplacedRobot() => r
   }
